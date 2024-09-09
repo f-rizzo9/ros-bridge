291b65bb6af8857e005d13df9bc92a8a672d53ac
f-rizzo9 <f.rizzo9@studenti.poliba.it>
2024-09-09 21:51:27 +0200

    final script

--- carla_ackermann_control/posture_regulator.py
+++ carla_ackermann_control/posture_regulator.py
@@ -15,9 +15,9 @@ k1_base = 1.8
 k2_base = 1
 k3_base = 1
 
-# Tresholds
-tresholdM = 1 
-tresholdR = 0.1
+# Thresholds
+thresholdM = 1 
+thresholdR = 0.1
 
 # Vehicle parameters
 l = 2.9  # Distance between the front and rear axles
@@ -25,11 +25,11 @@ max_steering_angle = math.radians(35)  # Physical limit of tesla model 3
 
 # State variables
 current_index = 2 # Set to 2 to discard fewer superfluous points that are too close to the vehicle at the beginning
-trajectory_points = []
+path_points = []
 current_pose = None
 xd, yd, yawd, yawd_prev = None, None, 0, 0
 x, y, yaw, yaw_prev = None, None, 0, 0
-trajectory_actual = []
+path_actual = []
 velocities = []
 simulation_complete = False
 steering_angles = []
@@ -63,21 +63,21 @@ def callback_current_pose(data):
     yaw_prev = yaw
     yaw = euler[2]
 
-    # Store actual trajectory
-    trajectory_actual.append((x, y))
+    # Store actual path
+    path_actual.append((x, y))
     
     rospy.loginfo("Current vehicle pose: x=%f, y=%f, yaw=%f", x, y, yaw)
 
 
 def callback_waypoints(data):
-    global trajectory_points
-    trajectory_points = data.poses
-    rospy.loginfo("Received %d trajectory points", len(trajectory_points))
+    global path_points
+    path_points = data.poses
+    rospy.loginfo("Received %d path points", len(path_points))
 
 def process_point():
     global current_index, xd, yd, yawd, yawd_prev
-    if current_index < len(trajectory_points):
-        pose = trajectory_points[current_index].pose
+    if current_index < len(path_points):
+        pose = path_points[current_index].pose
         xd = pose.position.x
         yd = pose.position.y
         orientation_q = pose.orientation
@@ -91,15 +91,15 @@ def process_point():
         )
         euler = tf.transformations.euler_from_quaternion(quaternion)
         yawd = euler[2]
-        rospy.loginfo("Trajectory point: xd=%f, yd=%f, yawd=%f", xd, yd, yawd)
+        rospy.loginfo("Path point: xd=%f, yd=%f, yawd=%f", xd, yd, yawd)
     else:
-        rospy.loginfo("All trajectory points have been processed.")
+        rospy.loginfo("All path points have been processed.")
 
 def is_approaching_curve(lookahead=15): # To detect when a vehicle is about to approach a curve (threshold of 0.1 rad, i.e. thresoldR variable at the beginning of the controller's code)
-    global current_index, trajectory_points, yaw
+    global current_index, path_points, yaw
 
-    if current_index + lookahead < len(trajectory_points):
-        next_pose = trajectory_points[current_index + lookahead].pose
+    if current_index + lookahead < len(path_points):
+        next_pose = path_points[current_index + lookahead].pose
 
         orientation_q = next_pose.orientation
 
@@ -120,15 +120,15 @@ def is_approaching_curve(lookahead=15): # To detect when a vehicle is about to a
     return False
 
 def ackermann_controller():
-    global rho, gamma, delta, tresholdM, tresholdR, k1
+    global rho, gamma, delta, thresholdM, thresholdR, k1
 
     rho = math.sqrt((xd - x)**2 + (yd - y)**2)
     gamma = normalize_angle(math.atan2((yd - y), (xd - x)) - yaw)
     delta = gamma - normalize_angle(yawd - yaw)
 
-    if current_index + 5 > len(trajectory_points): # To slow down the vehicle when it approaches the end of the trajectory
+    if current_index + 5 > len(path_points): # To slow down the vehicle when it approaches the end of the path
         k1 = 0.7
-        tresholdM = 0.25
+        thresholdM = 0.25
     else:
     # Adjust gains dynamically based on distance and angle
         k1 = k1_base * abs(rho)
@@ -137,8 +137,8 @@ def ackermann_controller():
 
     # Reduce speed and adjust thresholds during turns
     if is_approaching_curve() or abs(yaw - yawd) > 0.1: 
-        tresholdM = 1
-        tresholdR = 0.5 # When the vehicle is about to approach a curve or when it is inside one, we have chosen to increase the angular threshold so that points are taken a little further apart from each other to facilitate the maneuver (k1, k2 and k3 higher, as well as v and phi)
+        thresholdM = 1
+        thresholdR = 0.5 # When the vehicle is about to approach a curve or when it is inside one, we have chosen to increase the angular threshold so that points are taken a little further apart from each other to facilitate the maneuver (k1, k2 and k3 higher, as well as v and phi)
         k1 *= 0.8 # Reduce speed during turns
         k2 *= 3 # Increase of phi in the curves
         k3 *= 3 # Increase of phi in the curves
@@ -170,26 +170,26 @@ def stop_vehicle(pub):
     rospy.loginfo("Vehicle has stopped.")
 
 def plot_results():
-    global trajectory_points, trajectory_actual, velocities, steering_angles
+    global path_points, path_actual, velocities, steering_angles
 
     # Reference trajectory
-    ref_x = [p.pose.position.x for p in trajectory_points]
-    ref_y = [p.pose.position.y for p in trajectory_points]
+    ref_x = [p.pose.position.x for p in path_points]
+    ref_y = [p.pose.position.y for p in path_points]
 
     # Activate interactive mode
     plt.ion()
-    # Actual trajectory
-    actual_x, actual_y = zip(*trajectory_actual)
+    # Actual path
+    actual_x, actual_y = zip(*path_actual)
     
     # Plot
     plt.figure(figsize=(10, 10))
-    plt.plot(ref_x, ref_y, 'r-', label='Reference Trajectory')
-    plt.plot(actual_x, actual_y, 'b', label='Vehicle Trajectory')
+    plt.plot(ref_x, ref_y, 'r-', label='Reference Path')
+    plt.plot(actual_x, actual_y, 'b', label='Vehicle Path')
     plt.scatter(ref_x[0], ref_y[0], color='green', s=100, label='Starting point')
     plt.scatter(ref_x[-1], ref_y[-1], color='purple', s=100, label='Goal point')
     plt.xlabel('X')
     plt.ylabel('Y')
-    plt.title('Trajectories')
+    plt.title('Paths')
     plt.legend()
     plt.grid(True)
     plt.tight_layout()
@@ -222,7 +222,7 @@ def plot_results():
     plt.close('all')  # Closes all figures
 
 def main():
-    global current_index, trajectory_points, simulation_complete
+    global current_index, path_points, simulation_complete
 
     rospy.init_node('posture_regulator', anonymous=True)
     rospy.Subscriber('/carla/ego_vehicle/waypoints', Path, callback_waypoints)
@@ -232,10 +232,10 @@ def main():
     rate = rospy.Rate(50) # Frequency of 50 Hz (controller), slightly upper than odometry (topic) frequency 
 
     while not rospy.is_shutdown():
-        if trajectory_points:
+        if path_points:
             process_point()
             if x is not None and y is not None and yaw is not None and xd is not None and yd is not None and yawd is not None:
-                while abs(xd - x) > tresholdM or abs(yd - y) > tresholdM or abs(abs(yawd) - abs(yaw)) > tresholdR: # The angular condition with the "abs" function is used to avoid ambiguity when yaw is on the max/min threshold of the angular definition (+pi or -pi)
+                while abs(xd - x) > thresholdM or abs(yd - y) > thresholdM or abs(abs(yawd) - abs(yaw)) > thresholdR: # The angular condition with the "abs" function is used to avoid ambiguity when yaw is on the max/min threshold of the angular definition (+pi or -pi)
                     v, omega = ackermann_controller()
                     vel_cmd = Twist()
                     vel_cmd.linear.x = v 
@@ -245,13 +245,13 @@ def main():
                     pub.publish(vel_cmd)
                     rate.sleep()
                 current_index += 1
-                if current_index >= len(trajectory_points):
-                    rospy.loginfo("All trajectory points have been processed.")
+                if current_index >= len(path_points):
+                    rospy.loginfo("All path points have been processed.")
                     stop_vehicle(pub)
                     simulation_complete = True
                     break
         else:
-            rospy.loginfo("Waiting to receive trajectory points...")
+            rospy.loginfo("Waiting to receive path points...")
         rate.sleep()
 
     if simulation_complete:
